d012b991556952249757e4b5869e1f35e9d0262e
Aniket Shrivastava <aniketsrivastava57@gmail.com>
2026-05-05 17:54:24 +0530

    fix

--- agriculture/agriculture/settings.py
+++ agriculture/agriculture/settings.py
@@ -312,8 +312,8 @@ import cloudinary
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),  
-    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),       
-    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'), 
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),       
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
